18f65458e2ec0e0f2110d6bdcfaee6e9817d4ec6
Alex_D <shram.monolit@mail.ru>
2023-02-10 16:45:50 +0300

    Added validator

--- src/main/java/controller/UserControllerImpl.java
+++ src/main/java/controller/UserControllerImpl.java
@@ -1,5 +1,20 @@
 package controller;
 
-public class UserControllerImpl {
+import exception.ValidationException;
+import java.util.function.Function;
 
+public class UserControllerImpl {
+    private boolean validateUserName(String userName){
+        return (userName != null && !userName.equals(""));
+    }
+
+    private boolean validateId(Long id){
+        return (id != null && id > 0);
+    }
+
+    private <T> void validate(T object, Function<T, Boolean> validator, String errorMessage){
+        if (!validator.apply(object)){
+            throw new ValidationException(errorMessage);
+        }
+    }
 }
