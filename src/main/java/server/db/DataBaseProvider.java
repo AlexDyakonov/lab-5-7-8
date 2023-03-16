2c3212ff50e34a9656713e3dd87c94172c0a9327
Alex_D <shram.monolit@mail.ru>
2023-03-16 14:48:06 +0300

    Added generateNextId method, fixed load and save methods

--- src/main/java/server/db/DataBaseProvider.java
+++ src/main/java/server/db/DataBaseProvider.java
@@ -1,18 +1,18 @@
 package server.db;
 
-import server.exception.ApplicationException;
+import server.mapper.HumanBeingMapper;
 import server.model.HumanBeingModel;
 
 import java.io.*;
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.Set;
 
 import static client.ui.ConsoleColors.GREEN_BRIGHT;
 import static client.ui.ConsoleColors.RESET;
 
 public class DataBaseProvider {
-    private Set<HumanBeingModel> dataBase;
+    private final Set<HumanBeingModel> dataBase;
     private final LocalDateTime creationDate;
 
     public DataBaseProvider(String fileName) {
@@ -20,8 +20,10 @@ public class DataBaseProvider {
         this.creationDate = LocalDateTime.now();
     }
 
-    private Long generateNextId() {
-        return 1l;
+    private synchronized Long generateNextId() {
+        return dataBase.stream().mapToLong(HumanBeingModel::getId).max().isPresent()
+                ? dataBase.stream().mapToLong(HumanBeingModel::getId).max().getAsLong() + 1
+                : 1;
     }
 
     public Long addHumanToDatabase(HumanBeingModel model) {
@@ -32,23 +34,26 @@ public class DataBaseProvider {
     }
 
     private static Set<HumanBeingModel> loadDataBase(String fileName) {
-        Set<HumanBeingModel> resultSet;
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
-            resultSet = (LinkedHashSet<HumanBeingModel>) ois.readObject();
+        Set<HumanBeingModel> resultSet = new HashSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                resultSet.add(HumanBeingMapper.fromStringToHumanBeingModel(reader.readLine()));
+            }
             System.out.println(GREEN_BRIGHT + "Успешно загружено " + resultSet.size() + " элементов." + RESET);
         } catch (IOException e) {
-            throw new ApplicationException("load database from file *" + fileName + "+ error");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("load database from file *" + fileName + "+ error");
         }
         return resultSet;
     }
 
     public void save(String fileName) {
-        try (ObjectOutputStream bos = new ObjectOutputStream(new FileOutputStream(fileName))) {
-            bos.writeObject(dataBase);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (HumanBeingModel model : dataBase) {
+                writer.write(HumanBeingMapper.fromHumanBeingModelToStringLine(model));
+                writer.write(System.lineSeparator());
+            }
         } catch (IOException e) {
-            throw new ApplicationException("save to file *" + fileName +"* error");
+            System.out.println("save to file *" + fileName +"* error");
         }
     }
 
