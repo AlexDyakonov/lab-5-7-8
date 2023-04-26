df3065f3050f0fc50f97d68c899a555e4eed4724
Alex_D <shram.monolit@mail.ru>
2023-04-26 19:32:26 +0300

    Added logger

--- src/main/java/server/sql/PropertiesProvider.java
+++ src/main/java/server/sql/PropertiesProvider.java
@@ -5,16 +5,24 @@ import server.exception.FileException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import static client.ui.ConsoleColors.error;
+import static server.services.LoggerManager.setupLogger;
+import static util.Message.getLog;
 
 public class PropertiesProvider {
+    private static final Logger logger = Logger.getLogger(PropertiesProvider.class.getName());
     private static final Properties appProperties = new Properties();
 
     static {
+        setupLogger(logger);
         try {
+            logger.info(getLog("prop_provider_loading"));
             appProperties.load(new FileInputStream("src/main/resources/application.properties"));
+            logger.info(getLog("prop_provider_finish"));
         } catch (IOException e) {
+            logger.severe(e.getMessage());
             throw new FileException(error("Не найден файл application.properties. Для работы программы необходимо создать соответствующий файл."));
         }
     }
--- src/main/java/server/sql/SQLConnection.java
+++ src/main/java/server/sql/SQLConnection.java
@@ -1,25 +1,36 @@
 package server.sql;
 
-import server.exception.ApplicationException;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import static server.services.LoggerManager.setupLogger;
+import static util.Message.getLog;
 
 
 public class SQLConnection {
+    private static final Logger logger = Logger.getLogger(SQLConnection.class.getName());
     private final Connection connection;
+
+    static {
+        setupLogger(logger);
+    }
 
     public SQLConnection() {
         try {
             String url = PropertiesProvider.getAppProperties().getProperty("datasource.url");
+            logger.info(getLog("prop_url_got"));
             String username = PropertiesProvider.getAppProperties().getProperty("datasource.username");
+            logger.info(getLog("prop_username_got"));
             String password = PropertiesProvider.getAppProperties().getProperty("datasource.password");
+            logger.info(getLog("prop_pass_got"));
             connection = DriverManager.getConnection(url, username, password);
+            logger.info(getLog("connection_created"));
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            logger.severe("connection_not_created");
+            logger.severe(e.getMessage());
             throw new RuntimeException();
-//            throw new ApplicationException("Не удалось создать соединение");
         }
     }
 
