3c89fbb0c53d5c5c16786900813e5253e41bed39
Alex_D <shram.monolit@mail.ru>
2023-05-05 02:18:51 +0300

    [ADDED] Dependencies and GUI class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,32 @@
 import client.ui.ConsoleUI;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import server.commands.Invoker;
 import server.services.BuilderType;
 import util.LANGUAGE;
+import javafx.scene.control.Label;
 
 /**
  * The type App.
  */
-public class App {
+public class App extends Application {
 
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
+
     public static void main(String[] args) {
-        ConsoleUI session = new ConsoleUI(new Invoker(BuilderType.CMD, LANGUAGE.RU));
-        session.start();
+//        ConsoleUI session = new ConsoleUI(new Invoker(BuilderType.CMD, LANGUAGE.RU));
+//        session.start();
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ src/main/java/client/ui/GraphicUI.java
@@ -0,0 +1,4 @@
+package client.ui;
+
+public class GraphicUI {
+}
