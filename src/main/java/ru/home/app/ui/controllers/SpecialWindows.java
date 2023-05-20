ea08d670a54bc334462e47a2c7e04827ff83bd57
Alex_D <shram.monolit@mail.ru>
2023-05-20 20:46:57 +0300

    [ADDED] Confirmation window

---
+++ src/main/java/ru/home/app/ui/controllers/SpecialWindows.java
@@ -0,0 +1,27 @@
+package ru.home.app.ui.controllers;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.util.Optional;
+
+public class SpecialWindows {
+    public static boolean showConfirmationDialog(String message, Stage stage) {
+        stage.setAlwaysOnTop(false);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        ButtonType yesButton = new ButtonType("Yes");
+        ButtonType noButton = new ButtonType("No");
+
+        alert.getButtonTypes().setAll(yesButton, noButton);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == yesButton;
+    }
+}
