36142fbcad1026b0c08066f287073f063be79bc0
AlexDyakonov <shram.monolit@mail.ru>
2023-06-02 12:21:20 +0300

    ADDED THE BIGGEST SURPRISE

--- src/main/java/ru/home/app/gui/controllers/MainPageController.java
+++ src/main/java/ru/home/app/gui/controllers/MainPageController.java
@@ -21,8 +21,12 @@ import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.converter.DoubleStringConverter;
@@ -146,6 +150,24 @@ public class MainPageController implements Initializable {
             SpecialWindows.showError(getErrorMessagesGUI("fxml_error", localizationManager.getLanguage()) + "\n" + e.getMessage(),
                     getErrorMessagesGUI("fxml_error_title", localizationManager.getLanguage()));
         }
+        scene.setOnKeyPressed(e ->{
+            if (e.getCode() == KeyCode.F10){
+                StackPane root = new StackPane();
+
+                MediaPlayer player = new MediaPlayer( new Media(new File("src/main/resources/ru/home/app/video/rick-roll").toURI().toString()));
+                MediaView mediaView = new MediaView(player);
+
+                root.getChildren().add( mediaView);
+
+                Scene scene = new Scene(root, 1024, 768);
+
+                stage.setScene(scene);
+                stage.show();
+
+
+                player.play();
+            }
+        });
     }
 
     public void launchMainScene(Stage stage) {
